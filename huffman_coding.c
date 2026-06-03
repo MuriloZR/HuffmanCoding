a2c68280ee379bd83933c5e7e614f3339984e7a9
MuriloZR <murilodojogo@gmail.com>
2026-06-03 13:41:56 -0300

    initial commit

---
+++ huffman_coding.c
@@ -0,0 +1,11 @@
+/*
+=== TODO === :
+[] Implementar MinHeap, onde a frequência de cada caractere é o valor de prioridade
+[] Implementar a Huffman Tree
+
+=== ALGORITMO === :
+1. Colocar todos os caracteres na Heap
+2. Extrair os dois nós de menor frequência
+3. Criar um nó que tem freqência igual à soma das freqências dos dois nós recém extraídos
+4. Repetir os passos 2 e 3 até que a Heap possua apenas a raiz, a qual será a soma das freqências de todos os elementos
+*/
